8ec243b93b024336ae23d4d2b4c7070e98db5988
ayazadeh <m.ayazadeh@gmail.com>
2025-08-08 18:08:35 +0330

    Feat: add useMovieDetail composable to use movie detail service

---
+++ composables/movie/useMovieDetail.ts
@@ -0,0 +1,16 @@
+import { useMovieDetailService } from "./useMovie.service";
+
+export const useMovieDetail = (id: number) => {
+	if (!id) throw new Error("id is missing");
+
+	const getMovieDetail = useMovieDetailService();
+	const { data, pending, error } = useLazyAsyncData("movie-detail", () =>
+		getMovieDetail(id)
+	);
+
+	return {
+		data,
+		pending,
+		error,
+	};
+};
