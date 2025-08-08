fded51b5e6d25b7f3b611d3300c9352e1cd9e2dd
ayazadeh <m.ayazadeh@gmail.com>
2025-08-08 18:07:59 +0330

    Feat: add movie detail service to useMovie.service

--- composables/movie/useMovie.service.ts
+++ composables/movie/useMovie.service.ts
@@ -1,15 +1,28 @@
 import { useFetchApi } from "../api/useFetchApi";
 
 export const useMovieService = () => {
-    const $fetch = useFetchApi();
+	const $fetch = useFetchApi();
 
-    return async () => {
-        try {
-            const response = await $fetch('') 
+	return async () => {
+		try {
+			const response = await $fetch("");
+			return response;
+		} catch (e) {
+			console.error("Error in useMovieService: ", e);
+			throw e;
+		}
+	};
+};
+
+export const useMovieDetailService = () => {
+	const $fetch = useFetchApi();
+	return async (id: number) => {
+		try {
+			const response = await $fetch("", { params: { action: "info", id } });
             return response;
-        } catch (e) {
-            console.error('Error in useMovieService: ', e);
-            throw e;
-        }
-    }
-}
+		} catch (e) {
+			console.error("Error in useMovieDetailService: ", e);
+			throw e;
+		}
+	};
+};
