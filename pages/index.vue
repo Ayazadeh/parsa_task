f66c61369fa1dd3d63bd4e18dccae0291545d74b
ayazadeh <m.ayazadeh@gmail.com>
2025-08-08 18:09:30 +0330

    use useMovie and useMovieDetail composables in index page to check them

---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        {{ data }}
+        <br>
+        {{ detail }}
+    </div>
+</template>
+
+<script setup lang="ts">
+import { useMovieList } from '~/composables/movie/useMovie';
+import { useMovieDetail } from '~/composables/movie/useMovieDetail';
+
+const { data, pending } = useMovieList();
+
+const { data: detail } = useMovieDetail(2501);
+</script>
