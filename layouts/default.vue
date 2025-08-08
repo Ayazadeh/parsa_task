2e519787f5beaa0c505763310aa9bd2fa6f154f1
ayazadeh <m.ayazadeh@gmail.com>
2025-08-08 18:30:32 +0330

    Feat: add default layout

---
+++ layouts/default.vue
@@ -0,0 +1,13 @@
+<template>
+    <section class="flex flex-col min-h-screen">
+        <TheMenu />
+
+        <main class="mb-10">
+            <slot />
+        </main>
+
+        <footer></footer>
+    </section>
+</template>
+
+<script setup lang="ts"></script>
