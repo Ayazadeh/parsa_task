1106786c0d44123c4587d787d2b8a0e13b420d97
ayazadeh <m.ayazadeh@gmail.com>
2025-08-09 22:11:37 +0330

    Update: AppButton

--- components/AppButton.vue
+++ components/AppButton.vue
@@ -1,7 +1,10 @@
 <template>
 	<button
-		class="flex items-center justify-center rounded-sm"
-		:class="{ 'bg-[#1B1C1F] p-2 w-10 h-10': menu }"
+		class="flex items-center justify-center rounded-sm h-12"
+		:class="{
+			'bg-[#1B1C1F] p-2 w-12': menu,
+			'bg-[#242629] py-3 px-4': !menu,
+		}"
 	>
 		<slot></slot>
 	</button>
