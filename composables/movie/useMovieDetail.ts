c6eaf6ce40f2ef0c397154da43ca8eeb4d653722
ayazadeh <m.ayazadeh@gmail.com>
2025-08-09 22:12:45 +0330

    Fix: add some types to useMovieDetail

--- composables/movie/useMovieDetail.ts
+++ composables/movie/useMovieDetail.ts
@@ -4,9 +4,12 @@ export const useMovieDetail = (id: number) => {
 	if (!id) throw new Error("id is missing");
 
 	const getMovieDetail = useMovieDetailService();
-	const { data, pending, error } = useLazyAsyncData(`movie-detail-${id}`, () =>
-		getMovieDetail(id)
-	);
+	const {
+		data,
+		pending,
+		error,
+	}: { data: Record<string, any>; pending: any; error: any } =
+		useLazyAsyncData(`movie-detail-${id}`, () => getMovieDetail(id));
 
 	return {
 		data,
