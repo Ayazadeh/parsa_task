cc52ed2ea80954c33761d1cf62c75c8aaedfa24a
ayazadeh <m.ayazadeh@gmail.com>
2025-08-10 23:02:25 +0330

    update: improve the page

--- pages/index.vue
+++ pages/index.vue
@@ -9,52 +9,65 @@
 				</AppButton>
 			</div>
 		</section>
-		<section class="flex flex-row items-center justify-between h-16">
-			<div class="flex flex-row items-center gap-2">
+		<section class="grid grid-cols-12 grid-rows-1 gap-1 h-16">
+			<div class="col-span-7">
 				<ResolutionIcon />
 				<div class="flex flex-col">
 					<span>{{ detail?.title_en }}</span>
 					<span class="text-xs text-gray-400">{{ detail?.title_fa }}</span>
 				</div>
+				<div>
+					<AppButton> webdl - 1080p کیفیت </AppButton>
+				</div>
 			</div>
-			<div>
+			<div class="col-span-5">
+				<div class="flex flex-row items-center" dir="ltr">
+					<img
+						src="assets/images/imdb.png"
+						alt="imdb"
+						class="w-[24px] h-[24px] rounded-[2px]"
+					/>
+					<span class="text-xl font-bold">{{ detail?.imdb }}</span
+					>/10
+					<StarIcon />
+					<StarIcon />
+					<StarIcon />
+					<StarIcon />
+					<StarIcon />
+				</div>
 				<AppButton>
-					  webdl - 1080p کیفیت 
+					<FrameIcon />
+				</AppButton>
+				<AppButton class="w-[24px] h-[24px]">
+					<ShareIcon />
 				</AppButton>
 			</div>
-			<div></div>
-			<div></div>
-			<div></div>
-			<div></div>
 		</section>
-		<section class="flex flex-row items-center">
-			<!-- Video Player Section -->
-			<main class="flex-1 p-4">
-				<div
-					class="w-full aspect-video bg-gray-700 flex items-center justify-center"
-				>
-					<!-- Placeholder for video player -->
-					<p class="text-gray-400">Video Player Placeholder (2:26 / 8:04)</p>
+		<section class="overflow-hidden">
+			<div class="grid grid-cols-12 gap-3">
+				<!-- Video Player Section -->
+				<div class="relative mx-auto col-span-8 h-[456px] w-full">
+					<img :src="detail?.poster" alt="poster" class="w-full h-full" />
 				</div>
-			</main>
 
-			<!-- Sidebar -->
-			<aside class="w-64 bg-gray-800 p-4 overflow-y-auto">
-				<div
-					v-for="movie in movies"
-					:key="movie.title"
-					class="mb-4 flex items-center"
-				>
-					<img :src="movie.image" class="w-16 h-24 object-cover mr-2" />
-					<div>
-						<p class="text-sm font-semibold">{{ movie.title }}</p>
-						<p class="text-xs text-gray-400">{{ movie.quality }}</p>
-						<div class="flex items-center text-yellow-400 text-xs">
-							<span>★</span> <span>{{ movie.rating }}</span> / 10
+				<!-- Sidebar -->
+				<aside class="relative col-span-3 h-[456px]">
+					<div
+						v-for="movie in detail?.episodes"
+						:key="movie.title"
+						class="mb-4 flex items-center"
+					>
+						<img :src="movie.poster" class="w-16 h-24 object-cover mr-2" />
+						<div>
+							<p class="text-sm font-semibold">{{ movie.title }}</p>
+							<p class="text-xs text-gray-400">{{ movie.quality }}</p>
+							<div class="flex items-center text-yellow-400 text-xs">
+								<span>★</span> <span>{{ movie.rating }}</span> / 10
+							</div>
 						</div>
 					</div>
-				</div>
-			</aside>
+				</aside>
+			</div>
 		</section>
 	</div>
 </template>
@@ -63,6 +76,9 @@
 import { useMovieDetail } from "~/composables/movie/useMovieDetail";
 import ArrowLeftIcon from "~/components/icons/ArrowLeftIcon.vue";
 import ResolutionIcon from "~/components/icons/ResolutionIcon.vue";
+import StarIcon from "~/components/icons/StarIcon.vue";
+import ShareIcon from "~/components/icons/ShareIcon.vue";
+import FrameIcon from "~/components/icons/FrameIcon.vue";
 
 const { data: detail } = useMovieDetail(2501);
 
