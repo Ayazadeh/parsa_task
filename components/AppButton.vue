719c6b3c8ff45e03e2c2e68d6c4a9740108183ea
ayazadeh <m.ayazadeh@gmail.com>
2025-08-11 22:38:37 +0330

    Ref: move button styles to main.css

--- components/AppButton.vue
+++ components/AppButton.vue
@@ -1,9 +1,9 @@
 <template>
 	<button
-		class="flex items-center justify-center rounded-sm h-12"
+		class="flex items-center justify-center cursor-pointer"
 		:class="{
-			'bg-[#1B1C1F] p-2 w-12': menu,
-			'bg-[#242629] py-3 px-4': !menu,
+			'buttonMenu': menu,
+			'button': !menu,
 		}"
 	>
 		<slot></slot>
